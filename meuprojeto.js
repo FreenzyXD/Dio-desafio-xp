3fc439b2a82d9bb287360bf3db2050bdd3572d95
Freenzy <nicolasjanes69@gmail.com>
2023-10-25 23:16:59 -0300

    commit 2 estilizando e melhorando

--- meuprojeto.js
+++ meuprojeto.js
@@ -1,37 +1,46 @@
-let nome =prompt(" Qual seu nome de Herói?");
-let pontuacao=  parseInt(prompt( "de 0 a 10000 quão bom voce se classifica?"));
-let mensagem = "";
+let pessoas = parseInt(prompt("Quantos heróis são?"));
+let heroData = [];
+
+for (let i = 0; i < pessoas; i++) {
+    let nome = prompt("Qual é o nome do Herói?");
+    let pontuacao = parseInt(prompt("De 0 a 10000, quão bom você se classifica?"));
+    let mensagem = [""];
 
     switch (true) {
         case pontuacao > 0 && pontuacao <= 1000:
-            mensagem = "O Herói "+ nome + " esta no nível Ferro";
+            mensagem = "O Herói " + nome + " está no nível Ferro";
             break;
         case pontuacao > 1000 && pontuacao <= 2000:
-            mensagem ="O Herói "+ nome +  "esta no nível Bronze";
+            mensagem = "O Herói " + nome + " está no nível Bronze";
             break;
         case pontuacao > 2000 && pontuacao <= 5000:
-            mensagem ="O Herói "+ nome + " esta no nível Prata";
+            mensagem = "O Herói " + nome + " está no nível Prata";
             break;
         case pontuacao > 5000 && pontuacao <= 7000:
-            mensagem ="O Herói "+ nome + " esta no nível Ouro";
+            mensagem = "O Herói " + nome + " está no nível Ouro";
             break;
         case pontuacao > 7000 && pontuacao <= 8000:
-            mensagem ="O Herói "+ nome + " esta no nível Platina";
+            mensagem = "O Herói " + nome + " está no nível Platina";
             break;
         case pontuacao > 8000 && pontuacao <= 9000:
-            "O Herói "+ nome + " esta no nível Ascendente";
+            mensagem = "O Herói " + nome + " está no nível Ascendente";
             break;
         case pontuacao > 9000 && pontuacao <= 10000:
-            mensagem ="O Herói "+ nome + " esta no nível Imortal";
+            mensagem = "O Herói " + nome + " está no nível Imortal";
             break;
         case pontuacao > 10000:
-            mensagem ="O Herói "+ nome + " esta no nível Radiante";
+            mensagem = "O Herói " + nome + " está no nível Radiante";
             break;
-            default:
-            mensagem = "Voce não tem conhecimento algum"
+        default:
+            mensagem = "Você não tem conhecimento algum";
             break;
-            
-        
-        }
-        console.log(mensagem);
-        document.getElementById("greeting").innerHTML = mensagem
+
+    }
+    
+    console.log(mensagem);
+    heroData += mensagem + "<br>";
+}
+
+document.getElementById("greeting").innerHTML = heroData;
+
+
