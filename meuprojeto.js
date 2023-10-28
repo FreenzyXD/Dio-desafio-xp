895fad8fbc80b57bcfe6b119e8107e8eb6392435
Freenzy <nicolasjanes69@gmail.com>
2023-10-27 22:25:39 -0300

    projeto finalizado

--- meuprojeto.js
+++ meuprojeto.js
@@ -1,50 +1,47 @@
-document.getElementById("meuBotao").addEventListener("click", function inicio() {
-    alert("Botão clicado! Seu código JavaScript está funcionando.");
-});
+console.log(document.getElementById("meuBotao"))
+document.getElementById("meuBotao").addEventListener("click", inicio)
 function inicio (){
-let pessoas = parseInt(prompt("Quantos heróis são?"));
-let heroData = [];
+    let pessoas = parseInt(prompt("Quantos heróis são?"));
+    let heroData = [];
 
-for (let i = 0; i < pessoas; i++) {
-    let nome = prompt("Qual é o nome do Herói?");
-    let pontuacao = parseInt(prompt("De 0 a 10000, quão bom você se classifica?"));
-    let mensagem = [""];
+    for (let i = 0; i < pessoas; i++) {
+        let nome = prompt("Qual é o nome do Herói?");
+        let pontuacao = parseInt(prompt("De 0 a 10000, quão bom você se classifica?"));
+        let mensagem = [""];
 
-    switch (true) {
-        case pontuacao > 0 && pontuacao <= 1000:
-            mensagem = "O Herói " + nome + " está no nível Ferro";
-            break;
-        case pontuacao > 1000 && pontuacao <= 2000:
-            mensagem = "O Herói " + nome + " está no nível Bronze";
-            break;
-        case pontuacao > 2000 && pontuacao <= 5000:
-            mensagem = "O Herói " + nome + " está no nível Prata";
-            break;
-        case pontuacao > 5000 && pontuacao <= 7000:
-            mensagem = "O Herói " + nome + " está no nível Ouro";
-            break;
-        case pontuacao > 7000 && pontuacao <= 8000:
-            mensagem = "O Herói " + nome + " está no nível Platina";
-            break;
-        case pontuacao > 8000 && pontuacao <= 9000:
-            mensagem = "O Herói " + nome + " está no nível Ascendente";
-            break;
-        case pontuacao > 9000 && pontuacao <= 10000:
-            mensagem = "O Herói " + nome + " está no nível Imortal";
-            break;
-        case pontuacao > 10000:
-            mensagem = "O Herói " + nome + " está no nível Radiante";
-            break;
-        default:
-            mensagem = "Você não tem conhecimento algum";
-            break;
-
-    }
-    
+        switch (true) {
+            case pontuacao > 0 && pontuacao <= 1000:
+                mensagem = "O Herói " + nome + " está no nível Ferro";
+                break;
+            case pontuacao > 1000 && pontuacao <= 2000:
+                mensagem = "O Herói " + nome + " está no nível Bronze";
+                break;
+            case pontuacao > 2000 && pontuacao <= 5000:
+                mensagem = "O Herói " + nome + " está no nível Prata";
+                break;
+            case pontuacao > 5000 && pontuacao <= 7000:
+                mensagem = "O Herói " + nome + " está no nível Ouro";
+                break;
+            case pontuacao > 7000 && pontuacao <= 8000:
+                mensagem = "O Herói " + nome + " está no nível Platina";
+                break;
+            case pontuacao > 8000 && pontuacao <= 9000:
+                mensagem = "O Herói " + nome + " está no nível Ascendente";
+                break;
+            case pontuacao > 9000 && pontuacao <= 10000:
+                mensagem = "O Herói " + nome + " está no nível Imortal";
+                break;
+            case pontuacao > 10000:
+                mensagem = "O Herói " + nome + " está no nível Radiante";
+                break;
+            default:
+                mensagem = "Você não tem conhecimento algum";
+                break;
+        }
     console.log(mensagem);
     heroData += mensagem + "<br>";
-}
+    }
 
-document.getElementById("greeting").innerHTML = heroData;
+    document.getElementById("greeting").innerHTML = heroData;
 }
 
